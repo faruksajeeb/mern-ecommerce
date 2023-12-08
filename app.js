8aadce9ac2140da15022d8974a69543ad539e8da
Mohammad Omar Faruk <ofsajeeb@gmail.com>
2023-12-08 19:31:03 +0600

    front-en & back-end scafolding

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = new express();
+const bodyParser = require("body-parser");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+const cors = require("cors");
+
+// Datatabse
+const mongoose = require("mongoose");
+
+// Routes
+const router = require("./src/routes/api");
+
+//Security
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(hpp());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+app.use(bodyParser.json());
+
+// Rate Limiting
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 300 });
+app.use(limiter);
+
+// Database Connection
+// let URI="mongodb+srv://<username>:<password>@cluster0.7uslu.mongodb.net/CRUD4";
+// let OPTION={user:'testuser7777',pass:'testuser7777',autoIndex:true};
+// mongoose.connect(URI,OPTION).then((res)=>{
+//     console.log("Success")
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+
+// Front Routing
+app.use(express.static('client/build'));
+app.get('*',function(req,res){
+    req.sendFile(path.resolve(__dirname,'client','build','index.html'));
+});
+
+// Backend Routing
+app.use("/api", router);
+app.use("*", (req, res) => {
+  res.status(404).json({ status: "fail", data: "Not Found" });
+});
+
+module.exports = app;
---
+++ src/routes/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Api Routing end point
+
+
+module.exports = router;
